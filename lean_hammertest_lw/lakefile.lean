b388080a76fd1dc6a0bc542f7d5dfb1bd011912c
Jannis Limperg <limperg@amazon.com>
2025-11-30 17:23:03 +0100

    Update Auto for RPINF-free forward reasoning
    We update to an Aesop version that removes RPINF from forward reasoning.

--- lean_hammertest_lw/lakefile.lean
+++ lean_hammertest_lw/lakefile.lean
@@ -9,4 +9,4 @@ lean_lib Hammertest
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.20.0"
 
-require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "unify-tests"
+require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "redesign"
